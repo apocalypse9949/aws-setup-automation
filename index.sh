0516d06fb66f22b1601c248db1c8006f62cba084
prudhvi <125962989+apocalypse9949@users.noreply.github.com>
2025-08-07 19:11:35 +0530

    Update index.sh

--- index.sh
+++ index.sh
@@ -3,7 +3,7 @@ set -e
 
 # Ensure the script is run as root
 if [ "$(id -u)" -ne 0 ]; then
-  echo "ERROR: This script must be run as root. Try using: sudo ./setup.sh"
+  echo "ERROR: This script must be run as root. Try using: sudo ./index.sh"
   exit 1
 fi
 
@@ -20,32 +20,31 @@ ln -sf $JAVA_HOME/bin/javac /usr/bin/javac
 echo "Verifying Java installation:"
 java -version
 
-# Tomcat Installation
 echo "Downloading and extracting Apache Tomcat 9.0.89..."
 wget https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.89/bin/apache-tomcat-9.0.89.tar.gz
 tar -xvzf apache-tomcat-9.0.89.tar.gz
 mv apache-tomcat-9.0.89 tomcat9
 chmod +x tomcat9/bin/*.sh
 
-# Update Tomcat port
+
 echo "Updating Tomcat port from 8080 to 9090..."
 sed -i 's/port="8080"/port="9090"/' tomcat9/conf/server.xml
 
-# Configure Tomcat user roles
+
 echo "Adding Tomcat user roles and credentials..."
 sed -i '/<\/tomcat-users>/i \
 <role rolename="manager-gui"/>\n\
 <role rolename="manager-script"/>\n\
 <user username="admin" password="admin" roles="manager-gui,manager-script"/>' tomcat9/conf/tomcat-users.xml
 
-# Allow remote access to management interfaces
+
 echo "Enabling remote access to manager and host-manager applications..."
 for file in tomcat9/webapps/manager/META-INF/context.xml tomcat9/webapps/host-manager/META-INF/context.xml; do
   sed -i 's/<Valve /<!-- <Valve /' "$file"
   sed -i 's/\/> -->/\/> -->/' "$file"
 done
 
-# Start Tomcat
+
 echo "Starting Tomcat service..."
 cd tomcat9/bin
 export JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto.x86_64
@@ -56,13 +55,13 @@ cd ~
 echo "Tomcat is now running on port 9090."
 echo "Access it via: http://<your-ec2-ip>:9090"
 
-# Jenkins Installation
+
 echo "Installing Jenkins..."
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum install -y jenkins
 
-# Start Jenkins service
+
 echo "Starting and enabling Jenkins service..."
 systemctl start jenkins
 systemctl enable jenkins
